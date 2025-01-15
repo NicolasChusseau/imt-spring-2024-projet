47432e6474a8ec4b8756d2b3669d64e2088d6b73
pacom <pacomecailleteau@gmail.com>
2025-01-15 16:28:33 +0100

    Nouveaux Gets/Put/Delete + ajout des contraintes sur le format des données

--- TournamentMaster3000/src/main/java/org/imt/tournamentmaster/model/equipe/Equipe.java
+++ TournamentMaster3000/src/main/java/org/imt/tournamentmaster/model/equipe/Equipe.java
@@ -1,9 +1,9 @@
 package org.imt.tournamentmaster.model.equipe;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 import java.util.Objects;
@@ -13,11 +13,15 @@ public class Equipe {
 
     @JsonIgnore
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank(message = "Le nom de l'équipe est obligatoire")
+    @Size(max = 100, message = "Le nom de l'équipe ne doit pas dépasser 100 caractères")
     private String nom;
 
     @OneToMany
+    @Size(min = 1, max = 5, message = "Une équipe doit contenir entre 1 et 5 joueurs")
     private List<Joueur> joueurs;
 
     public Equipe() {
